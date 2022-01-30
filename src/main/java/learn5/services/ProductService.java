df4371d0c8c427bd2cf1846e95e89a68fd119e14
Жахонгир <jahon.080879@gmail.com>
2022-01-30 20:27:11 +0500

    Add @TransactionalManagament into Config class

--- src/main/java/learn5/services/ProductService.java
+++ src/main/java/learn5/services/ProductService.java
@@ -3,6 +3,7 @@ package learn5.services;
 import learn5.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -10,7 +11,8 @@ public class ProductService {
     @Autowired
     ProductRepository productRepository;
 
-
+    @Transactional
+    // rollsback the RuntimeExceptions but doesnot rollback the checked exceptions
     public void addOneProduct(){
         productRepository.addProduct("Beer");
         throw new RuntimeException(":(");
