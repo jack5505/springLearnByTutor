756c88953f30b9205b33aadb919c603ceca22236
Жахонгир <jahon.080879@gmail.com>
2022-01-30 20:00:14 +0500

    Add @TransactionalManagament into Config class

--- src/main/java/learn5/config/ProjectConfig.java
+++ src/main/java/learn5/config/ProjectConfig.java
@@ -8,17 +8,19 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"learn5.repository","learn5.services"})
+@EnableTransactionManagement
 public class ProjectConfig {
 
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setUsername("postgresql");
+        driverManagerDataSource.setUsername("postgres");
         driverManagerDataSource.setPassword("admin");
         driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/learn");
         return driverManagerDataSource;
--- src/main/java/learn5/services/ProductService.java
+++ src/main/java/learn5/services/ProductService.java
@@ -3,13 +3,16 @@ package learn5.services;
 import learn5.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
     @Autowired
     ProductRepository productRepository;
 
+
     public void addOneProduct(){
         productRepository.addProduct("Beer");
+        throw new RuntimeException(":(");
     }
 }
